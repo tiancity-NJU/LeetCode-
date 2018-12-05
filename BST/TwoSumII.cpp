8bb73265c15ac909c97fca60fc85c7b4287f61a4
sumiya-NJU <32325193+sumiya-NJU@users.noreply.github.com>
2018-12-05 19:44:38 +0800

    Update TwoSumII.cpp

--- BST/TwoSumII.cpp
+++ BST/TwoSumII.cpp
@@ -3,8 +3,10 @@
 最常用的问题是对一个有序数组或者列表的查询，利用递归的方法,这样可以保证运行时间在O(lgn)：
 
 */
-
- void BST(vector<int>&nums,int s,int e,int target,int& res)
+class Solution {
+public:
+    
+    void BST(vector<int>&nums,int s,int e,int target,int& res)
     {
         if(s>=e) return;
         int mid=(e+s)/2;
@@ -15,3 +17,18 @@
         
         
     }
+    
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        
+        int remain;
+        int find=-1;
+        for(int i=0;i<numbers.size();i++)
+        {
+            remain=target-numbers[i];
+            BST(numbers,i,numbers.size(),remain,find);
+            if(find!=-1){ans.push_back(i+1);ans.push_back(find+1);break;}
+        }
+        return ans;
+    }
+};
