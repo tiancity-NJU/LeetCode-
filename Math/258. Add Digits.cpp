9ef8172413cff41e66c1c5c19e9d83f77a7e6e99
sumiya-NJU <32325193+sumiya-NJU@users.noreply.github.com>
2018-12-06 14:56:14 +0800

    Create 258. Add Digits.cpp

---
+++ Math/258. Add Digits.cpp
@@ -0,0 +1,27 @@
+
+/*
+
+  无聊的题目
+
+
+*/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        
+  
+        while(num>9)
+        {
+            int tmp=0;
+            while(num!=0)
+            {
+                tmp+=num%10;
+                num/=10;
+            }
+            num=tmp;
+        }
+        
+        return num;
+    }
+};
