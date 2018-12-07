0d3f3ac7df7842a67d6a5718157923770d89e2b8
YuPeize <455205287@qq.com>
2018-12-07 16:45:05 +0800

    Update 18. 4Sum.cpp

--- Array/18. 4Sum.cpp
+++ Array/18. 4Sum.cpp
@@ -1,12 +1,9 @@
 /*
-
   套路和3sum一样      外面再套一层循环构成  1+3 sum   注意碰到相同的要跳过去
   另外可以加一下限制条件，跳过不可能情况，进一步缩短运行时间，例如排序后最后一个数为MAX
   如果 第一个数+3*X<target 则直接跳过   这样以后能够打败93.3%的用户
-
+  author :ztc
 */
-
-
 
 class Solution {
 public:
@@ -51,6 +48,42 @@ public:
                     }
                 }
             }
+        }
+        return ans;
+    }
+};
+
+
+/*
+author: ypz
+*/
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        if(nums.size() < 4) return ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 3; i++) {
+            int z = nums[i];
+            if(i > 0 && z == nums[i - 1]) continue;
+            for(int m = i + 1; m < nums.size() - 2; m++) {
+                int a = nums[m];
+                if(m > i + 1 && a == nums[m - 1]) continue;
+                for(int j = m + 1, k = nums.size() - 1; j < k; ) {
+                    int b = nums[j];
+                    int c = nums[k];
+                    vector<int> element = {z, a, b, c};
+                    if(z + a + b + c == target) {
+                        ans.push_back(element);
+                        while(j < k && b == nums[++j]);
+                        while(j < k && c == nums[--k]);
+                    }
+                    else if(z + a + b + c > target) k--;
+                    else j++;
+                }
+                
+            }
+
         }
         return ans;
     }
