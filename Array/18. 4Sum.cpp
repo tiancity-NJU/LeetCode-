6368f8a34649ae94029b01b2e536c445628a27b5
sumiya-NJU <32325193+sumiya-NJU@users.noreply.github.com>
2018-12-07 15:00:12 +0800

    Update 18. 4Sum.cpp

--- Array/18. 4Sum.cpp
+++ Array/18. 4Sum.cpp
@@ -1,6 +1,8 @@
 /*
 
   套路和3sum一样      外面再套一层循环构成  1+3 sum   注意碰到相同的要跳过去
+  另外可以加一下限制条件，跳过不可能情况，进一步缩短运行时间，例如排序后最后一个数为MAX
+  如果 第一个数+3*X<target 则直接跳过   这样以后能够打败93.3%的用户
 
 */
 
@@ -14,10 +16,12 @@ public:
         if(nums.size()<4) return ans;
         
         sort(nums.begin(),nums.end());
-        
+        int MAX=nums.back();
+   
         for(int i=0;i<nums.size()-3;i++)
         {
             if(i!=0&&nums[i]==nums[i-1]) continue;
+            if(nums[i]+3*MAX<target) continue;
             for(int j=i+1;j<nums.size()-2;j++)
             {
                 if(j!=i+1&&nums[j]==nums[j-1]) continue;
