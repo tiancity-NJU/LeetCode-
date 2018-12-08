fe14c61df6e824973fafd4888b354d2886765689
sumiya-NJU <32325193+sumiya-NJU@users.noreply.github.com>
2018-12-08 18:45:36 +0800

    Update 34. Find First and Last Position of Element in Sorted Array.cpp

--- BST/34. Find First and Last Position of Element in Sorted Array.cpp
+++ BST/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -38,3 +38,48 @@ public:
         return ans;
     }
 };
+
+
+/*
+  还有一种思路就是调用两次非递归的二分查找，经过测试，耗时基本差不多，相对快一点点
+  首先我们需要找左边界，这时采用  int mid=left+(right-left)/2;  使得 mid 更加靠近 left，这样至于每次 nums[mid]<target 的时候才移动left，其他
+  时候全部移动right，因为如果nums[mid]==target 可能还不是最左边的，因此需要移动right，让其更加靠近左边界
+  另外注意while的条件判断不能出现等于，否则，   right=mid 这一句就无限循环了
+*/
+
+
+class Solution {
+public:
+    
+  
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        ans.push_back(-1);
+        ans.push_back(-1);
+        if(nums.size()==0) return ans;
+        int left=0,right=nums.size()-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]<target)
+                left=mid+1;
+            else right=mid;
+        }
+        if(nums[left]!=target) return ans;
+        ans[0]=left;
+        left=0,right=nums.size()-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2+1;
+            if(nums[mid]>target)
+                right=mid-1;
+            else left=mid;
+        }
+        
+        ans[1]=right;
+        return ans;
+        
+        
+    }
+};
