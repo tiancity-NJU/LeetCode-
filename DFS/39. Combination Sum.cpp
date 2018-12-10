c506cb656c89c5d6b907f543ab36e31b77862f77
YuPeize <455205287@qq.com>
2018-12-10 12:20:22 +0800

    Create 39. Combination Sum.cpp

---
+++ DFS/39. Combination Sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    /*
+    By backtracking
+    author: ypz
+    */
+    
+    void backTracking(vector<vector<int>>& ans, vector<int>& candidates, vector<int>& cur, int temp, int target) {
+        if(temp == target) {
+            ans.push_back(cur);
+            return;
+        }
+        else if(temp > target || candidates.size() == 0) return;
+        else {
+            for(int i = 0; i < candidates.size(); i++) {
+                vector<int> list(candidates);
+                vector<int> curr(cur);
+                int tmp = temp;
+                if(i != 0) list.erase(list.begin(), list.begin() + i);
+                curr.push_back(list[0]);
+                tmp += list[0];
+                backTracking(ans, list, curr, tmp, target);
+            }
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        if(candidates.size() == 0) return ans;
+        vector<int> cur;
+        int temp = 0;
+        backTracking(ans, candidates, cur, temp, target);
+        return ans;
+    }
+};
