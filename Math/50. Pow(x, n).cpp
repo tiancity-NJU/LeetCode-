e76e578c64d72d263f775d29d15720dbe2af41ce
YuPeize <455205287@qq.com>
2018-12-12 16:42:51 +0800

    Create 50. Pow(x, n).cpp

---
+++ Math/50. Pow(x, n).cpp
@@ -0,0 +1,14 @@
+/*
+每次算他的一半
+author: ypz
+*/
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+        double half = myPow(x, n/2);
+        if(n % 2 == 0) return half * half;
+        else if(n > 0) return half * half * x;
+        else return half * half / x;
+    }
+};
