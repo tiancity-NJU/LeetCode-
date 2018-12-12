1d50cfa6c6c71cd88ea523b0759d92171a8578c1
YuPeize <455205287@qq.com>
2018-12-12 15:51:55 +0800

    Update 3. Longest Substring Without Repeating Characters.cpp

--- String/3. Longest Substring Without Repeating Characters.cpp
+++ String/3. Longest Substring Without Repeating Characters.cpp
@@ -1,12 +1,8 @@
-
 /*
-
   第一种是相对暴力的方法，维护一个“纯净”的字符串result，每次都在result里面查找新到来字符的位置，如果找不到，就直接加进去，如果找到了，更新
   最长长度，然后该位置（包括自己）之前的所有元素，最后别忘乐再比较一次用来处理尾端的情况。
-
-
+  author: ztc
 */
-
 
 class Solution {
 public:
@@ -43,19 +39,12 @@ int lengthOfLongestSubstring(string s) {
 }
 };
 
-
-
-
-
 /*
-
   上面的方法相对比较野蛮，再次看到这个问题想到利用hash表也可以做，可以维护一个字符到出现该字符最后位置索引的一个映射
   如果当前索引减去当前长度 i-len> hs[s[i]] 或者hs[s[i]]为空，都说明在当前考虑的子串里，s[i]第一次出现，更新hs[s[i]]
   否则更新len和hs[s[i]]，具体方法如下
-
+  author: ztc
 */
-
-
 
 class Solution {
 public:
@@ -73,5 +62,28 @@ public:
             i++;
         }
         return max(maxLen,len);
+    }
+};
+
+
+/*
+author: ypz
+*/
+
+class Solution {
+public: 
+    int romanToInt(string s) {
+        string s_string = "IVXLCDM";
+        vector<int> s_int {1,5,10,50,100,500,1000};
+        int sum = 0;
+        for(int i = 0; i < s.size(); i++){
+            int index1 = s_string.find(s[i]);
+            sum += s_int[index1];
+            if(index1 % 2 == 0){
+                int index2 = s_string.find(s[i + 1]);
+                if(index1 < index2) sum += s_int[index1] * (-2);
+            }
+        }
+        return sum;
     }
 };
