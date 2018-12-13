a3c007c042fd33172c644df9375cf7736f816399
Peize Yu <455205287@qq.com>
2018-12-13 11:11:27 +0800

    Update 69. Sqrt(x).cpp

--- Math/69. Sqrt(x).cpp
+++ Math/69. Sqrt(x).cpp
@@ -11,12 +11,8 @@ public:
         while(left <= right) {
             long long mid = (left + right) / 2;
             if(mid * mid == x) return mid;
-            else if(mid * mid > x) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
+            else if(mid * mid > x) right = mid - 1;
+            else left = mid + 1;
         }
         return right;
     }
