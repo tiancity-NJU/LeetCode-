f4522e6616156e75ed4648d0c280e88bbaef2a8a
Peize Yu <455205287@qq.com>
2018-12-15 20:03:08 +0800

    Update 78. Subsets.cpp

--- DFS/78. Subsets.cpp
+++ DFS/78. Subsets.cpp
@@ -1,10 +1,5 @@
-
-
-
 /*
-
   典型的DFS问题，这种问题和求所有排列组合的问题一样，较为通用的框架为
-  
   DFS(data, sIndex,eIndex,int goalCondition,&tmpResult,&Result)
   {
             if(sIndex>eIndex) return;
@@ -14,12 +9,9 @@
             
             DFS(data,sIndex+1,eIndex,goalCondition,tmpResult,Result)
   }
-
-
   另外该类问题还可以通过 bitmap解决
-
+  author: ztc
 */
-
 
 class Solution {
 public:
@@ -43,8 +35,6 @@ public:
 	comb(list,s + 1, e, len,result,condition);
 }
 
-
-
 vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         for(int i=0;i<=nums.size();i++)
@@ -58,8 +48,45 @@ vector<vector<int>> subsets(vector<int>& nums) {
     }
 };
 
-
 /*
     利用bitMap解决,但是这样只能解决特定大小内的数组长度
 
 */
+
+
+
+
+/*
+By backtracking.
+author: ypz
+*/
+
+class Solution {
+public:
+    
+    void backTracking(vector<vector<int>> &ans, vector<int> &curr, vector<int> &nums, int k) {
+        if(curr.size() == k) ans.push_back(curr);
+        else {
+            if(nums.size() == 0) return;
+            for(int i = 0; i < nums.size(); i++) {
+                vector<int> curr_temp(curr);
+                vector<int> nums_temp(nums);
+                curr_temp.push_back(nums[i]);
+                nums_temp.erase(nums_temp.begin(), nums_temp.begin() + i + 1);
+                backTracking(ans, curr_temp, nums_temp, k);
+            }
+
+        }
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        for(int i = 0; i <= nums.size(); i++) {
+            vector<int> curr_temp_temp(curr);
+            vector<int> nums_temp_temp(nums);
+            backTracking(ans, curr_temp_temp, nums_temp_temp, i);
+        }
+        return ans;
+    }
+};
