9b7de05c9ab45b78821c02f8b60e1384800ad57e
Peize Yu <455205287@qq.com>
2018-12-16 23:12:52 +0800

    Create 88. Merge Sorted Array.cpp

---
+++ Array/88. Merge Sorted Array.cpp
@@ -0,0 +1,30 @@
+/*
+author: ypz
+*/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0;
+        int j = 0;
+        while(i <= m || j <= n) {
+            if(i == m && j == n) return;
+            else if(i < m && j == n) i++;
+            else if(i == m && j < n) {
+                nums1.insert(nums1.begin() + i, nums2[j]);
+                nums1.erase(nums1.end() - 1);
+                i++;
+                m++;
+                j++;
+            }
+            else if(nums2[j] <= nums1[i]) {
+                nums1.insert(nums1.begin() + i, nums2[j]);
+                nums1.erase(nums1.end() - 1);
+                i++;
+                m++;
+                j++;
+            }
+            else i++;
+        }
+    }
+};
