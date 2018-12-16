33d5fe51c255ff4af3b01af99bcc6a703aa9cb5a
Peize Yu <455205287@qq.com>
2018-12-16 22:48:12 +0800

    Update 82. Remove Duplicates from Sorted List II.cpp

--- List/82. Remove Duplicates from Sorted List II.cpp
+++ List/82. Remove Duplicates from Sorted List II.cpp
@@ -1,13 +1,8 @@
-
-
-
 /*
   本实现还不是最优的，增加了一些无用的条件判断，但是对于时间复杂度没有造成影响，解决List的问题，特别是涉及到头指针修改的问题，常用思路
   就是增加一个临时头指针指向head，这样就将整个List同等对待
-  
-  
+  author: ztc  
 */
-
 
 /**
  * Definition for singly-linked list.
@@ -41,7 +36,45 @@ public:
             
         }
         return new_head->next;
-        
-       
+    }
+};
+
+
+/*
+author: ypz
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL) return head;
+        ListNode* dummy = new ListNode(0);
+        dummy -> next = head;
+        ListNode* curr = dummy;
+        ListNode* temp = curr -> next;
+        while(temp != NULL && temp -> next != NULL) {
+            if(temp -> next -> val == temp -> val){
+                ListNode* nxxt = temp -> next;
+                while(nxxt != NULL && nxxt -> val == temp -> val) {
+                    nxxt = nxxt -> next;
+                    temp = temp -> next;
+                }
+                curr -> next = nxxt;
+                temp = curr -> next;
+            }
+            else {
+                curr = curr -> next;
+                temp = curr -> next;
+            }
+        }
+        return dummy -> next;
     }
 };
