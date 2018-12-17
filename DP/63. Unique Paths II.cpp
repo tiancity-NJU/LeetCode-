78c57ce6c49d8910bffe74f4f54a1010ab51cfb8
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-17 20:03:59 +0800

    Update 63. Unique Paths II.cpp

--- DP/63. Unique Paths II.cpp
+++ DP/63. Unique Paths II.cpp
@@ -33,3 +33,36 @@ public:
         return ans[m-1][n-1];
     }
 };
+
+
+/*
+    这种智能右移和下移的问题，可以转化为 m+1 * n+1 矩阵的DP问题，也可以进行空间的压缩变成长度为n的DP问题
+*/
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        if(obstacleGrid.size()==0) return 0;
+    
+        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
+        vector<int>DP(n,0);
+        for(int i=0;i<n;i++) 
+            if(obstacleGrid[0][i]==0) DP[i]=1;
+            else break;
+        
+        for(int i=1;i<m;i++)
+        {
+            if(DP[0]==1&&obstacleGrid[i][0]!=1) DP[0]=1;
+            else DP[0]=0;
+            for(int j=1;j<n;j++)
+            {
+                if(obstacleGrid[i][j]==1){DP[j]=0;continue;}
+                DP[j]=DP[j-1]+DP[j];
+            }
+        } 
+        return DP.back();
+        
+        
+    }
+};
