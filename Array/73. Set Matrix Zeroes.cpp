59ae2b933224fbfc3d8c0e09bc58c434b9e3394c
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-17 20:28:33 +0800

    Create 73. Set Matrix Zeroes.cpp

---
+++ Array/73. Set Matrix Zeroes.cpp
@@ -0,0 +1,43 @@
+
+
+/*
+  对于该题 O(m+n) 的空间复杂度是很好想的，利用两个一维数组 row col 先扫描一遍数组，对应的row col位置打上标记，再循环一遍即可
+  但是对于O(1) 想了很久，后来才发现，只需要将数组的第一行和第一列作为上述的row col即可，非0即保留
+*/
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int> > &matrix) {
+        if (matrix.empty() || matrix[0].empty()) return;
+        int m = matrix.size(), n = matrix[0].size();
+        bool rowZero = false, colZero = false;
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) colZero = true;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (matrix[0][i] == 0) rowZero = true;
+        } 
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (rowZero) {
+            for (int i = 0; i < n; ++i) matrix[0][i] = 0;
+        }
+        if (colZero) {
+            for (int i = 0; i < m; ++i) matrix[i][0] = 0;
+        }
+    }
+};
