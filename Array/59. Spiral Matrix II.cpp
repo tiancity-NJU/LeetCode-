a76fc556f9dc6c87ab4c4fd2aca518f29503ae00
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-17 19:26:01 +0800

    Update 59. Spiral Matrix II.cpp

--- Array/59. Spiral Matrix II.cpp
+++ Array/59. Spiral Matrix II.cpp
@@ -55,3 +55,42 @@ public:
         return ans;
     }
 };
+
+
+/*
+    显然可以发现，每次都是沿着顺时针方向改变，并且步数是  n n-1 n-1 n-2 n-2 ....1 1
+*/
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<int>tmp(n,0);
+        vector<vector<int>>ans(n,tmp);
+        
+        int direct[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+
+        int step=n;
+        int num=1;
+        int i=0,j=0,k=0;
+        int x=0,y=n-1,d=1;
+        for(i=0;i<step;i++)
+            ans[0][i]=num++;
+        
+        step--;
+        int remain=n*n-n;
+        while(step)
+        {
+            for(j=0;j<2;j++)
+            {
+                for(k=0;k<step;k++)
+                {
+                    x+=direct[d][0];
+                    y+=direct[d][1];
+                    ans[x][y]=num++;
+                }
+                d=(d+1)%4;
+            }
+            step--;
+        }
+        return ans;
+    }
+};
