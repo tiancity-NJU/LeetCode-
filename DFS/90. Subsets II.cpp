38549554952baac8afd52a74c6360bdae046ff76
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-17 22:58:57 +0800

    Update 90. Subsets II.cpp

--- DFS/90. Subsets II.cpp
+++ DFS/90. Subsets II.cpp
@@ -28,3 +28,35 @@ public:
         return ans;
     }
 };
+
+/*
+  和上面方法差不多，对于重复元素，一定要区分： 答案里面存在重复元素 比如 [1,2,2]  但是不能存在 [1,2,5] [1,2,5] 因此在每一层选取的时候
+  不能选同一个数，不同层，可以选不同的数
+  核心就是    sort       if(i!=s&&nums[i]==nums[i-1]) continue;
+*/
+
+class Solution {
+public:
+    
+    void comb(vector<int>&nums,int s,vector<vector<int>>&ans,vector<int>&tmp)
+    {
+        ans.push_back(tmp);
+        
+        for(int i=s;i<nums.size();i++)
+        {
+            if(i!=s&&nums[i]==nums[i-1]) continue;
+            tmp.push_back(nums[i]);
+            comb(nums,i+1,ans,tmp);
+            tmp.pop_back();
+        }
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        vector<int>tmp;
+        comb(nums,0,ans,tmp);
+        
+        return ans;
+    }
+};
