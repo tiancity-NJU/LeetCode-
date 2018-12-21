3f346166651ded113cba2db89a9a0c80a1113a8e
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-21 14:19:41 +0800

    Update 122. Best Time to Buy and Sell Stock II.cpp

--- Array/122. Best Time to Buy and Sell Stock II.cpp
+++ Array/122. Best Time to Buy and Sell Stock II.cpp
@@ -14,3 +14,30 @@ public:
         return ans;
     }
 };
+
+/*
+    可以当天卖然后当天买，以为不能在同一天卖买，i+=1 和 i+=2的区别
+    也可以直接扫描，只要前后两天是增序，就加上差值
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        if(prices.size()==0) return 0;
+        int i=1;
+        
+        int count=0;
+        while(i<prices.size())
+        {
+            while(i<prices.size()&&prices[i]<prices[i-1]) i++;
+            if(i>=prices.size()) break;
+            
+            count+=prices[i]-prices[i-1];
+            i+=1;
+        }
+        
+        return count;
+        
+    }
+};
