3ee7af7d234828d22b5f45997c2308a15a32effc
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-21 13:44:55 +0800

    Update 121. Best Time to Buy and Sell Stock.cpp

--- Array/121. Best Time to Buy and Sell Stock.cpp
+++ Array/121. Best Time to Buy and Sell Stock.cpp
@@ -14,3 +14,30 @@ public:
         return maxProfit;
     }
 };
+
+
+/*
+    以上问题可以用DP的方法做，虽然对于该题，没必要浪费O(n) 的空间，但是会是 一种更加通用的思想，只不过DP的每一个值只依赖于前面一个
+
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        vector<int>delta(prices.size(),0);
+        
+        if(prices.size()==0) return 0;
+        
+        vector<int>buy(prices.size(),0),sell(prices.size(),0);
+        buy[0]=prices[0];
+        for(int i=1;i<prices.size();i++)
+        {
+            buy[i]=min(prices[i],buy[i-1]);
+            sell[i]=max(prices[i]-buy[i-1],sell[i-1]);
+        }
+        
+        
+        return sell.back();
+    }
+};
