ae6ed7d18a6a8a97c51f9bebad039c4e33aaf878
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-22 16:50:04 +0800

    Update 5. Longest Palindromic Substring.cpp

--- String/5. Longest Palindromic Substring.cpp
+++ String/5. Longest Palindromic Substring.cpp
@@ -53,6 +53,49 @@ public:
 
 
 /*
+  或者直接在里面保存max，但是这样容易出错，可以作为上述方法的代码优化，时间复杂度还是一致的，但是对于大规模数据，运行时间缩小了一倍
+*/
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+        if(s.size()==0) return s;
+        vector<vector<bool>>DP(s.size(),vector<bool>(s.size(),false));
+        int mi=0,mj=1;
+        int maxLen=1;
+        for(int i=0;i<s.size();i++)   
+        {
+            DP[i][i]=1;
+            if(i!=0&&s[i-1]==s[i]) 
+            {
+                DP[i-1][i]=1;
+                if(maxLen<2){maxLen=2;mi=i-1;mj=i;}
+            }
+        }
+        for(int len=3;len<=s.size();len++)
+        {
+            for(int i=0;i+len-1<s.size();i++)
+            {
+                int j=i+len-1;
+                if(s[i]==s[j]&&DP[i+1][j-1]) 
+                {
+                    DP[i][j]=1;
+                    if(len>maxLen)
+                    {
+                        maxLen=len;
+                        mi=i;mj=j;
+                    }
+                }
+            }
+        }
+       return s.substr(mi,maxLen);
+     
+    }
+};
+
+
+/*
     还有一个经典算法 Manacher 专门用来解决最长回文问题，时间复杂度 O(n)
 
 */
