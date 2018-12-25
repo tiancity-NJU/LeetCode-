7bf9aab8cd4392d5b8485c436e608282bef848c3
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-25 20:08:52 +0800

    Create 373. Find K Pairs with Smallest Sums.cpp

---
+++ Array/373. Find K Pairs with Smallest Sums.cpp
@@ -0,0 +1,50 @@
+
+
+
+
+/*
+  一般这种找几个前K个最值的问题都可以转化为堆问题，堆最大的麻烦就是自定义比较函数，参考了网上的priority_queue 的代码
+*/
+
+
+class Solution {
+public:
+   
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        using p = std::pair<int, int>;
+        auto cmp = [](const auto& p1, const auto& p2)   // 最大堆
+        { 
+            return p1.first + p1.second < p2.first + p2.second;
+        };
+        
+        std::priority_queue<p, std::vector<p>, std::function<bool(p, p)>> pq(cmp);
+        
+        int l1 = std::min(k, (int)nums1.size()), l2 = std::min(k, static_cast<int>(nums2.size()));
+        
+        for (int i = 0; i < l1; ++i)
+        {
+            for (int j = 0; j < l2; ++j)
+            {
+                int n1 = nums1[i], n2 = nums2[j];
+                if (pq.size() < k)
+                {
+                    pq.push(std::make_pair(n1, n2));
+                }
+                else if (n1 + n2 < pq.top().first + pq.top().second)
+                {
+                    pq.pop();
+                    pq.push(std::make_pair(n1, n2));
+                }
+            }
+        }
+     
+        std::vector<std::pair<int, int>> ret;
+        while (!pq.empty())
+        {
+            ret.emplace_back(pq.top());
+            pq.pop();
+        }
+        return ret;
+    }
+    
+};
