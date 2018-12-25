a9d8185d76564e04975ea2be2dfa45943c67b281
Peize Yu <455205287@qq.com>
2018-12-25 23:32:24 +0800

    Update 191. Number of 1 Bits.cpp

--- BIT/191. Number of 1 Bits.cpp
+++ BIT/191. Number of 1 Bits.cpp
@@ -1,13 +1,11 @@
 /*
   位运算问题，n每次右移一位和1进行与运算
+  author: ztc
 */
-
 
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        
-        
+    int hammingWeight(uint32_t n) { 
         int count=0;
         for(int i=0;i<32;i++)
         {
@@ -15,13 +13,12 @@ public:
             n=n>>1;
         }
         return count;
-        
     }
 };
 
-
 /*
     还有一种思路是每次把最低位的1变成0，然后count++， n&(n-1) 即可
+    author: ztc
 */
 
 class Solution {
@@ -33,5 +30,22 @@ public:
             ++count;
         }
         return count;
+    }
+};
+
+
+/*
+author: ypz
+*/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            if(n & 1 == 1) ans++;
+            n = n >> 1;
+        }
+        return ans;
     }
 };
