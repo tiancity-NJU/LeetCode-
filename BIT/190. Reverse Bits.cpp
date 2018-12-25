91f2851ddc73358eb765452b40e2c3c5d634f532
Peize Yu <455205287@qq.com>
2018-12-25 23:13:41 +0800

    Update 190. Reverse Bits.cpp

--- BIT/190. Reverse Bits.cpp
+++ BIT/190. Reverse Bits.cpp
@@ -1,7 +1,7 @@
 
 /*
   简单的位运算问题，注意顺序不能反
-
+  author: ztc
 */
 
 class Solution {
@@ -16,5 +16,20 @@ public:
             n=n>>1;
         }
         return ans;
+    }
+};
+
+
+/*
+author: ypz
+*/
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        bitset<32> n_bitset(n);
+        bitset<32> ans;
+        for(auto i = 0; i < 32; i++) ans[i] = n_bitset[32 - i - 1];
+        return ans.to_ulong();
     }
 };
