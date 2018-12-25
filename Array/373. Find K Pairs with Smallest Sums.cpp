974896b2e6ee84efa008d6adcf1ffdfcbb3f9cfe
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2018-12-25 20:12:16 +0800

    Update 373. Find K Pairs with Smallest Sums.cpp

--- Array/373. Find K Pairs with Smallest Sums.cpp
+++ Array/373. Find K Pairs with Smallest Sums.cpp
@@ -11,6 +11,45 @@ class Solution {
 public:
    
     vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        using p = pair<int, int>;
+        auto cmp = [](const auto& p1, const auto& p2)   // 最大堆
+        { 
+            return p1.first + p1.second > p2.first + p2.second;
+        };
+        
+        priority_queue<p, vector<p>, function<bool(p, p)>> pq(cmp);
+        
+        int l1 = min(k, (int)nums1.size()), l2 = min(k, static_cast<int>(nums2.size()));
+        
+        for (int i = 0; i < l1; ++i)
+        {
+            for (int j = 0; j < l2; ++j)
+            {
+                int n1 = nums1[i], n2 = nums2[j];  
+                pq.push(make_pair(n1, n2));
+            }
+        }
+     
+        std::vector<pair<int, int>> ret;
+        while (!pq.empty()&&k--)
+        {
+            ret.emplace_back(pq.top());
+            pq.pop();
+        }
+        return ret;
+    }
+    
+};
+
+/*
+
+  可以优化的方法就是建立最大堆，比最大堆打就删除堆顶，永远保证堆里只有K个元素
+*/
+
+class Solution {
+public:
+   
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         using p = std::pair<int, int>;
         auto cmp = [](const auto& p1, const auto& p2)   // 最大堆
         { 
