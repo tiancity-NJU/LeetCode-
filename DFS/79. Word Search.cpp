f0cf61442d24d752e3f0107d837369996d150036
Peize Yu <455205287@qq.com>
2018-12-26 20:18:01 +0800

    Update 79. Word Search.cpp

--- DFS/79. Word Search.cpp
+++ DFS/79. Word Search.cpp
@@ -1,22 +1,15 @@
-
-
-
-
 /*
   典型DFS，时间复杂度没得法子减，空间上图方便，利用了一个mask，表示不能走或者走过的路，然后朝着四个方向深度遍历
   但是完全可以将走过的位置换成一个特殊的字符，深度以后用string word对应位置的字符复原即可
+  author: ypz
 */
 
-
 class Solution {
-public:
-    
+public:   
     void dfs(vector<vector<char>>&board,vector<vector<int>>&mask,int x,int y,string& remain,bool& find)
     {
         if(board[x][y]!=remain[0]) return;
-        
-        if(board[x][y]==remain[0]&&remain.size()==1) {find=true;return;}
-        
+        if(board[x][y]==remain[0]&&remain.size()==1) {find=true;return;} 
             mask[x][y]=1;
             string rem=remain.substr(1);
             if(x+1<board.size()&&!mask[x+1][y]) dfs(board,mask,x+1,y,rem,find);
@@ -28,15 +21,12 @@ public:
             if(y-1>=0&&!mask[x][y-1]) dfs(board,mask,x,y-1,rem,find);
             if(find) return;
             mask[x][y]=0;
-        
     }
-    
     
     bool exist(vector<vector<char>>& board, string word) {
         vector<int> tmp(board[0].size(),0);
         vector<vector<int>>mask(board.size(),tmp);
         bool find=false;
-        
         for(int i=0;i<board.size();i++)
         {
             for(int j=0;j<board[0].size();j++)
@@ -49,6 +39,36 @@ public:
             }
         }
         return false;
-        
+    }
+};
+
+
+/*
+By backtracking.
+flag用来存储是否访问过，pos为word的字母索引。
+author: ypz
+*/
+
+class Solution {
+public:
+    bool backTracking(vector<vector<char>>& board, vector<vector<bool>>& flag, int pos, int i, int j, string word) {
+        if(pos == word.size()) return true;
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size()) return false;
+        if(flag[i][j]) return false;
+        if(board[i][j] != word[pos]) return false;
+        flag[i][j] = true;
+        bool ans = backTracking(board, flag, pos+1, i+1, j, word) || backTracking(board, flag, pos+1, i-1, j, word) || backTracking(board, flag, pos+1, i, j+1, word) || backTracking(board, flag, pos+1, i, j-1, word);
+        flag[i][j] = false;
+        return ans;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        vector<vector<bool>> flag(board.size(), vector<bool>(board[0].size(), false));
+        for(int i = 0; i < board.size(); i++) {
+            for(int j = 0; j < board[0].size(); j++) {
+                if(backTracking(board, flag, 0, i, j, word)) return true;
+            }
+        }
+        return false;
     }
 };
