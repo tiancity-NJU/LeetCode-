089e4058b495848334e5bd04a2992c84584195a8
Peize Yu <455205287@qq.com>
2018-12-27 20:39:21 +0800

    Create 110. Balanced Binary Tree.cpp

---
+++ BST/110. Balanced Binary Tree.cpp
@@ -0,0 +1,26 @@
+/*
+左右子树中的节点需均满足高度差不小于1.
+author: ypz
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getLength(TreeNode* root) {
+        if(root == NULL) return 0;
+        else return max(getLength(root -> left), getLength(root -> right)) + 1;
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        return abs(getLength(root -> left) - getLength(root -> right)) < 2  && isBalanced(root -> left) && isBalanced(root -> right);
+    }
+};
