dea29b964e0e17876847da679778cfeb36216d07
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2019-01-08 19:39:41 +0800

    Update 32. Longest Valid Parentheses.cpp

--- String/32. Longest Valid Parentheses.cpp
+++ String/32. Longest Valid Parentheses.cpp
@@ -22,3 +22,28 @@ public:
         return ans;
     }
 };
+
+
+/*
+        DP的方法
+*/
+
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int result=0;
+        s=')'+s;
+        vector<int> dp(s.length(),0);
+        for(int i=1;i<s.length();i++)
+        {
+            if(s[i]==')')
+            {
+                if(s[i-1-dp[i-1]]=='(') dp[i]=dp[i-1]+2;
+                dp[i]+=dp[i-dp[i]];
+            }
+            result=max(result,dp[i]);
+        }
+        return result;
+    }
+};
