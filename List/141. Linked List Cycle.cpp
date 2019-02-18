a928b47c9eca1b15b16eb8d1839154d579101dbf
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2019-02-18 13:03:41 +0800

    Update 141. Linked List Cycle.cpp

--- List/141. Linked List Cycle.cpp
+++ List/141. Linked List Cycle.cpp
@@ -29,3 +29,38 @@ public:
         return ans;
     }
 };
+
+
+/*
+    一个指针跑一步，一个跑两步，相遇则有环，Floyd算法
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr){
+            return false;
+        }
+        
+        ListNode *fast = head->next;
+        ListNode *slow = head->next;
+        
+        while(slow != nullptr && fast != nullptr && fast->next != nullptr){
+            
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                return true;
+            }
+        }      
+        return false;
+    }
+};
