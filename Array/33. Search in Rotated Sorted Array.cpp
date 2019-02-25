8780c3977ee8a79e4328606013e9d5225cb5df30
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2019-02-25 20:28:17 +0800

    Update 33. Search in Rotated Sorted Array.cpp

--- Array/33. Search in Rotated Sorted Array.cpp
+++ Array/33. Search in Rotated Sorted Array.cpp
@@ -29,3 +29,31 @@ public:
         else return -1;
     }
 };
+
+/*
+  思想不难理解，利用二分，mid左右必然有一个是有序的，对有序情况进行判断，不满足直接进入另一部分
+*/
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int len = nums.size(), left = 0, right = len-1;
+        while(left <= right)
+        {
+            int mid = (left+right)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] >= nums[0])
+            {
+                if(target < nums[mid] && target >= nums[0]) right = mid-1;
+                else left = mid+1;
+            }
+            else
+            {
+                if(target > nums[mid] && target < nums[0]) left = mid+1;
+                else right = mid-1;
+            }
+        }
+        return -1;
+    }
+};
