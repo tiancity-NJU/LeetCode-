01452ea74adfda2764a147cb41d75bbc866bb3ef
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2019-03-16 14:57:20 +0800

    Update 139. Word Break.cpp

--- DP/139. Word Break.cpp
+++ DP/139. Word Break.cpp
@@ -25,3 +25,36 @@ public:
         return dp[s.size()];
     }
 };
+
+
+/*
+
+        典型dp问题，重点区别好dp的下标和数组下标之间的关系
+
+*/
+
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int>dp(s.size()+1,0);
+        dp[0]=1;
+        for(int i=0;i<s.size();i++)
+        {
+            for(int j=0;j<wordDict.size();j++)
+            {
+                int len=wordDict[j].size();
+                if(len>i+1) continue;
+             
+                if(dp[i+1-len] && s.substr(i-len+1,len)==wordDict[j])
+                {
+                  
+                    dp[i+1]=1;
+                    break;
+                }
+            }
+        }
+       
+        return dp.back();
+    }
+};
