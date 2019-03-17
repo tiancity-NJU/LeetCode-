a5da9b8f0840bf898cfec483b74604049d0d5141
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2019-03-17 16:08:26 +0800

    Update 123. Best Time to Buy and Sell Stock III.cpp

--- DP/123. Best Time to Buy and Sell Stock III.cpp
+++ DP/123. Best Time to Buy and Sell Stock III.cpp
@@ -23,3 +23,51 @@ public:
         return sell2;
     }
 };
+
+
+/*
+       易理解的普适性解法
+       设定一个k+1 * 2 的dp数组，0表示进行第k次的买，1代表第k次的卖，对于每一天的价格都要遍历一次dp，确保在这一天进行每一次交易的情况
+*/
+
+
+class Solution {
+public:
+    
+ 
+    
+    int deal(vector<int>&prices,int k)
+    {
+        int ans=0;
+        if(2*k>=prices.size())
+        {
+            for(int i=1;i<prices.size();i++)
+            {
+                ans+=prices[i]>prices[i-1]?prices[i]-prices[i-1]:0;
+            }
+            return ans;
+        }
+        
+        vector<vector<int>>DP(k+1,vector<int>(2,INT_MIN));
+        DP[0][0]=0;
+        DP[0][1]=0;
+        
+        for(int i=0;i<prices.size();i++)
+        {
+            for(int j=1;j<k+1;j++)
+            {
+                DP[j][0]=max(DP[j][0],DP[j-1][1]-prices[i]);
+                DP[j][1]=max(DP[j][1],DP[j][0]+prices[i]);
+            }
+        }
+  
+        return DP.back()[1];  
+    }
+   
+    int maxProfit(vector<int>& prices) {
+        
+        return deal(prices,2);
+        
+        
+    }
+};
