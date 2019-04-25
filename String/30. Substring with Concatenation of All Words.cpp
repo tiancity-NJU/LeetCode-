4547346076036ac390660b8eb91f4d31db299c62
Peize Yu <455205287@qq.com>
2019-04-25 15:24:09 +0800

    Update 30. Substring with Concatenation of All Words.cpp

--- String/30. Substring with Concatenation of All Words.cpp
+++ String/30. Substring with Concatenation of All Words.cpp
@@ -1,10 +1,9 @@
 
 /*
+  author: ztc
   比较直观的，就是对每一个位置开始的字符串进行检查，由于长度相等，在第二层循环的时候可以简化一部分的计算，但是还是会出现大量重复计算
   （或许可以利用KMP等方法的思路进行优化）
-
 */
-
 
 class Solution {
 public:
@@ -23,7 +22,6 @@ public:
             else alpha[c]++;
         }
         
-        
         int len=words[0].size(),num=words.size(),total=s.size();
         for(int i=0;i<total-num*len+1;i++)
         {
@@ -35,6 +33,40 @@ public:
                 if(--seen[s.substr(i+j,len)]==-1) break;
             }
             if(j==num*len) ans.push_back(i);
+        }
+        return ans;
+    }
+};
+
+/*
+author: ypz
+*/
+
+class Solution {
+public:   
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> ans;
+        if(words.size() == 0) return ans;
+        int len = words.size();
+        int length = words[0].size();
+        if(s.size() < length) return ans;
+        unordered_map<string, int> count;
+        for(int i = 0; i < len; i++)
+            count[words[i]]++;
+        for(int i = 0; i < s.size() - len * length + 1; i++) {
+            unordered_map<string, int> seen;
+            int j = 0;
+            for(j; j < len; j++) {
+                string temp = s.substr(i + j * length, length);
+                auto it = find(words.begin(), words.end(), temp);
+                if(it != words.end()) {
+                    seen[*it]++;
+                    if(seen[*it] > count[*it])
+                        break;
+                }
+                else break;
+            }
+            if(j == len) ans.push_back(i);
         }
         return ans;
     }
