3ef5121fef3c1427e4395cb4971f7874e41041be
Drenccc <455205287@qq.com>
2019-05-31 13:20:01 +0800

    Update 120. Triangle.cpp

--- DP/120. Triangle.cpp
+++ DP/120. Triangle.cpp
@@ -1,17 +1,13 @@
 
 /*
+    author: ztc
     挺好的一个题目，包括前面的很多问题，正向求解会有难度，利用反向的扫描会变得更加简单，这题可以看作不同支流汇聚的问题，我们可以设置一个和triangle
     一样大小的成梯度的DP数组，其中 DP[i][j]代表从下往上到第i层第j个数的最短路径，这种问题同样可以用DFS，但是肯定很慢
-
 */
-
-
 
 class Solution {
 public:
-    
     int minimumTotal(vector<vector<int>>& triangle) {
-       
         if(triangle.size()==0||triangle[0].size()==0) return 0;
         vector<int>DP(triangle.back());
         for(int i=triangle.size()-2;i>=0;i--)
@@ -25,16 +21,12 @@ public:
     }
 };
 
-
 /*
-      还有一种暴力的解法，就是使用DFS，但是这样会超时
+    还有一种暴力的解法，就是使用DFS，但是这样会超时
 */
-
-
 
 class Solution {
 public:
-   
     void DFS(vector<vector<int>>&triangle,int pos,int s,int e,int& sum,int& res)
     {
         if(s==e)
@@ -44,7 +36,6 @@ public:
             sum-=min(triangle[e][pos],triangle[e][pos+1]);
             return;
         }
-        
         sum+=triangle[s][pos];
         DFS(triangle,pos,s+1,e,sum,res);
         sum-=triangle[s][pos];
@@ -53,9 +44,7 @@ public:
         sum-=triangle[s][pos+1];
     }
     
-    
     int minimumTotal(vector<vector<int>>& triangle) {
-       
         if(triangle.size()==0||triangle[0].size()==0) return 0;
         if(triangle.size()==1) return triangle[0][0];
         int res=INT_MAX;
@@ -63,4 +52,43 @@ public:
         DFS(triangle,0,1,triangle.size()-1,sum,res);
         return res;
     }
+};
+
+
+/*
+author: ypz
+*/
+
+// 方法一：自底向上，dp为二维数组
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if(triangle.size() == 1) 
+            return triangle[0][0];
+        vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size(), 0));
+        dp[triangle.size()-1] = triangle[triangle.size()-1];
+        for(int i = triangle.size() - 2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++) {
+                dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j];
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+
+// 方法二：自底向上，dp为一维数组，每一层循环对其更新
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> dp(triangle.back());
+        for(int i = triangle.size() - 2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++)
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
+        }
+        return dp[0];
+    }
+    
 };
