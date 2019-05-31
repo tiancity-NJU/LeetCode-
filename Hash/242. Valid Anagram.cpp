e87f564b37e3781a142b14047fd6ed3cab9fa28d
Drenccc <455205287@qq.com>
2019-05-31 14:28:45 +0800

    Update 242. Valid Anagram.cpp

--- Hash/242. Valid Anagram.cpp
+++ Hash/242. Valid Anagram.cpp
@@ -1,11 +1,7 @@
 /*
-
+  author: ztc
   题目意思没太明白。。一开始想复杂了，就是统计字符数目可一致
-
-
 */
-
-
 
 class Solution {
 public:
@@ -17,6 +13,26 @@ public:
         
         for (int i = 0; i < t.size(); i++) {
             if (--m[t[i] - 'a'] < 0) return false;
+        }
+        return true;
+    }
+};
+
+/*
+author: ypz
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> list;
+        for(int i = 0; i < s.size(); i++)
+            list[s[i]]++;
+        for(int i = 0; i < t.size(); i++)
+            list[t[i]]--;
+        for(int i = 0; i < list.size(); i++) {
+            if(list[i] != 0)
+                return false;
         }
         return true;
     }
