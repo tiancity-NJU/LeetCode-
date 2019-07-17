2628624a4339901ed3bdcebf90bb511f4697b345
Drenccc <455205287@qq.com>
2019-07-17 17:26:43 +0800

    Update 150. Evaluate Reverse Polish Notation.cpp

--- Stack/150. Evaluate Reverse Polish Notation.cpp
+++ Stack/150. Evaluate Reverse Polish Notation.cpp
@@ -13,7 +13,7 @@ public:
                 int x1 = con.top(); // 注意x1,x2不可颠倒，否则会出现除数为0
                 con.pop();
                 int x = 0;
-                switch(s[0]) {
+                switch(s[0]) {      // 注意swich里面必须是整型，若为s会报错
                     case '+': x = x1 + x2; break;
                     case '-': x = x1 - x2; break;
                     case '*': x = x1 * x2; break;
