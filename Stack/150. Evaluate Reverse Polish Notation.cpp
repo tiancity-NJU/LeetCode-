393d7b35c46c7a573625b83aaf7d62db94f26479
Drenccc <455205287@qq.com>
2019-07-17 17:25:22 +0800

    Create 150. Evaluate Reverse Polish Notation.cpp

---
+++ Stack/150. Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,27 @@
+// author: ypz
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> con;
+        for(auto s:tokens) {
+            if(s != "+" && s != "-" && s != "*" && s != "/")
+                con.push(stoi(s));
+            else { 
+                int x2 = con.top();
+                con.pop();
+                int x1 = con.top(); // 注意x1,x2不可颠倒，否则会出现除数为0
+                con.pop();
+                int x = 0;
+                switch(s[0]) {
+                    case '+': x = x1 + x2; break;
+                    case '-': x = x1 - x2; break;
+                    case '*': x = x1 * x2; break;
+                    case '/': x = x1 / x2; break;
+                }
+                con.push(x);
+            }
+        }
+        return con.top();
+    }
+};
