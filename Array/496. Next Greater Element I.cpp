e78ac58db0bda2f3062d67179e89f802d3378212
Drenccc <455205287@qq.com>
2019-07-25 20:15:05 +0800

    Update 496. Next Greater Element I.cpp

--- Array/496. Next Greater Element I.cpp
+++ Array/496. Next Greater Element I.cpp
@@ -34,3 +34,32 @@ public:
         return ans;
     }
 };
+
+/*
+author：ypz
+方法二：采用栈和哈希表
+借助一个栈和一个哈希表，当右值小于左值时入栈，否则出栈，哈希表用来存储该值右边第一个大值。
+*/
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        unordered_map<int, int> m;
+        vector<int> ans;
+        for(int i = 0; i < nums2.size(); i++) {
+            while(s.size() && s.top() < nums2[i]) {
+                m[s.top()] = nums2[i];
+                s.pop();
+            }
+            s.push(nums2[i]);
+        }
+        for(int i = 0; i < nums1.size(); i++) {
+            if(m.count(nums1[i]) != 0)
+                ans.push_back(m[nums1[i]]);
+            else
+                ans.push_back(-1);
+        }
+        return ans;
+    }
+};
